0373dd0024d6bc701319a811bab8974ed56bf716
rajarathod214 <rajarathod214@yahoo.com>
2017-08-22 14:15:03 +0530

    Program for Singly Linklist with header file

---
+++ header.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void create();
+struct emp *reverse(struct emp *);
+struct emp *recursive_reverse(struct emp *);
+void display();
+void add_node_at_begining();
+void add_node_after(int);
+void delete_node(int);
+
+void create_list(int);
+void display();
+void add_at_begining(int);
+void add_after(int,int);
+void Delete(int);
